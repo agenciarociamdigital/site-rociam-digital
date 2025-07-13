82b3c02f3ebc7ee2394fad75f8eb03ec49c385f9
Seu Nome <rociamdigitalmarketing@gmail.com>
2025-07-13 20:57:36 -0300

    google tag manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,6 +31,21 @@ export default defineNuxtConfig({
         { rel: 'apple-touch-icon', href: '/favicon.png' },
         { rel: 'shortcut icon', href: '/favicon.ico' },
         { rel: 'manifest', href: '/manifest.json' }
+      ],
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TQVTFS7');`,
+          type: 'text/javascript'
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQVTFS7" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
+        }
       ]
     }
   }
